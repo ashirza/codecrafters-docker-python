dca9214cc7a7e6610fccbd597c69d0bc8064727f
ashir <ashirza@gmail.com>
2023-03-27 18:36:59 +0100

    test

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     
     completed_process = subprocess.run([command, *args], capture_output=True)
     if completed_process.stdout:
-        print(completed_process.stdout.decode("utf-8"))
+        print(completed_process.stdout)
     # elif completed_process.stderr:
     #     print(completed_process.stderr.decode("utf-8"), file=sys.stderr)
 
