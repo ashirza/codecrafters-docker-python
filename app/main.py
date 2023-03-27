7dcb0bb17f83b9264ee89d79acf3b2928667f287
ashir <ashirza@gmail.com>
2023-03-27 18:52:24 +0100

    test

--- app/main.py
+++ app/main.py
@@ -7,15 +7,20 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    if completed_process.stdout:
-        stdout = completed_process.stdout.decode("utf-8")
-        stdout = stdout.replace("\n", "")
-        print(stdout)
-    elif completed_process.stderr:
-        stderr = completed_process.stderr.decode("utf-8")
-        stderr = stderr.replace("\n", "")
-        print(stderr, file=sys.stderr)
+    try:
+        completed_process = subprocess.run([command, *args], capture_output=True)
+    except subprocess.CalledProcessError as e:
+        exit_code = e.returncode
+        sys.exit(exit_code)        
+    finally:
+        if completed_process.stdout:
+            stdout = completed_process.stdout.decode("utf-8")
+            stdout = stdout.replace("\n", "")
+            print(stdout)
+        elif completed_process.stderr:
+            stderr = completed_process.stderr.decode("utf-8")
+            stderr = stderr.replace("\n", "")
+            print(stderr, file=sys.stderr)
 
 
 if __name__ == "__main__":
