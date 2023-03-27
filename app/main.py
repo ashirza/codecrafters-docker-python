4baef72759cf38784e7697b608bb5aab84a97037
ashir <ashirza@gmail.com>
2023-03-27 18:33:52 +0100

    stage 2

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ import subprocess
 import sys
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
+def main():    
+    # pipe programs stdout and stderr to parent process
     command = sys.argv[3]
     args = sys.argv[4:]
     
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    if completed_process.stdout:
+        print(completed_process.stdout.decode("utf-8"))
+    elif completed_process.stderr:
+        print(completed_process.stderr.decode("utf-8"), file=sys.stderr)
 
 
 if __name__ == "__main__":
