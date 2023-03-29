0cf54c299c4e78b48e352f8438bfa5f86d892364
ashir <ashirza@gmail.com>
2023-03-29 21:36:25 +0100

    new change

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ def main():
         if command == "ls":
             tempfile_path = tempfile.mkdtemp()
             shutil.copy("/usr/local/bin/docker-explorer", tempfile_path)
+            os.mkdir(tempfile_path + args[-1])
             # os.chroot(tempfile_path)
             os.chdir(tempfile_path)
             command = os.path.basename(os.path.normpath(command))
